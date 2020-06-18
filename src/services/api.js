5326361dc43819bc65e6f1eaed4bfea8ddc072dc
pGaldino25 <pgaldino25@gmail.com>
2020-06-18 16:45:08 -0300

    Exclusao de arquivos duplicados

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://192.168.1.38:3333'
-    // baseURL: 'http://pi-u7c.anonymous.mobile.exp.direct:80'
-    
-});
-
-export default api;
